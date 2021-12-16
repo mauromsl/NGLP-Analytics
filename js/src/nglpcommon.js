6bd13191b19a1cf68774f78af7d395fcc4769779
Aga Domanska <aga@cottagelabs.com>
2021-12-16 12:02:12 +0000

    add missing if after adding the google font

--- js/src/nglpcommon.js
+++ js/src/nglpcommon.js
@@ -9,7 +9,7 @@ export function extractPalette(sheetName, paletteSelector) {
         if (sheet.href && sheet.href.includes(sheetName)) {
             for (let j = 0; j < sheet.rules.length; j++) {
                 let rule = sheet.rules[j];
-                if (rule.selectorText && rule.selectorText.startsWith(paletteSelector + " ")) {
+                if (rule.selectorText && rule.selectorText && rule.selectorText.startsWith(paletteSelector + " ")) {
                     let key = rule.selectorText.substring(paletteSelector.length + 2);
                     palette[key] = rule.style.color;
                 }
